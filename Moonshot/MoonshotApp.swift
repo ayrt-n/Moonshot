50a69d55011a98b44ebb63bacb30d92a21ee1aaf
ayrton <ayrton.parkinson1@gmail.com>
2024-07-18 08:45:06 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ayrton Parkinson on 2024/07/18.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
