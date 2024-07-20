4c5db81158e37f2cb320076a615baf7ac1294086
ayrton <ayrton.parkinson1@gmail.com>
2024-07-20 07:00:01 -0400

    Split out crew member slider view

---
+++ Moonshot/CrewMemberSlider.swift
@@ -0,0 +1,70 @@
+//
+//  CrewMemberSlider.swift
+//  Moonshot
+//
+//  Created by Ayrton Parkinson on 2024/07/20.
+//
+
+import SwiftUI
+
+struct CrewMemberSlider: View {
+    let mission: Mission
+    let crew: [CrewMember]
+    
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(crew, id: \.role) { crewMember in
+                    NavigationLink {
+                        AstronautView(astronaut: crewMember.astronaut)
+                    } label: {
+                        HStack{
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width:104, height: 72)
+                                .clipShape(.capsule)
+                                .overlay(
+                                    Capsule()
+                                        .strokeBorder(.white, lineWidth: 1)
+                                )
+                            
+                            VStack(alignment: .leading) {
+                                Text(crewMember.astronaut.name)
+                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                                Text(crewMember.role)
+                                    .foregroundStyle(.white.opacity(0.5))
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+            }
+        }
+    }
+    
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing\(member.name)")
+            }
+        }
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    return CrewMemberSlider(mission: missions[0], astronauts: astronauts)
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+}
--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -9,12 +9,7 @@ import SwiftUI
 
 struct MissionView: View {
     let mission: Mission
-    let crew: [CrewMember]
-    
-    struct CrewMember {
-        let role: String
-        let astronaut: Astronaut
-    }
+    let astronauts: [String: Astronaut]
     
     var body: some View {
         ScrollView {
@@ -46,53 +41,13 @@ struct MissionView: View {
                 }
                 .padding(.horizontal)
                 
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(crew, id: \.role) { crewMember in
-                            NavigationLink {
-                                AstronautView(astronaut: crewMember.astronaut)
-                            } label: {
-                                HStack{
-                                    Image(crewMember.astronaut.id)
-                                        .resizable()
-                                        .frame(width:104, height: 72)
-                                        .clipShape(.capsule)
-                                        .overlay(
-                                            Capsule()
-                                                .strokeBorder(.white, lineWidth: 1)
-                                        )
-                                    
-                                    VStack(alignment: .leading) {
-                                        Text(crewMember.astronaut.name)
-                                            .foregroundStyle(.white)
-                                            .font(.headline)
-                                        Text(crewMember.role)
-                                            .foregroundStyle(.white.opacity(0.5))
-                                    }
-                                }
-                                .padding(.horizontal)
-                            }
-                        }
-                    }
-                }
+                CrewMemberSlider(mission: mission, astronauts: astronauts)
             }
             .padding(.bottom)
         }
         .navigationTitle(mission.displayName)
         .navigationBarTitleDisplayMode(.inline)
         .background(.darkBackground)
-    }
-    
-    init(mission: Mission, astronauts: [String: Astronaut]) {
-        self.mission = mission
-        
-        self.crew = mission.crew.map { member in
-            if let astronaut = astronauts[member.name] {
-                return CrewMember(role: member.role, astronaut: astronaut)
-            } else {
-                fatalError("Missing\(member.name)")
-            }
-        }
     }
 }
 
